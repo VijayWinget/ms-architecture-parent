bcd69f8758556fdf55b5542bcc2243fcb3eaf9ae
VijayWinget <gvvijay82@gmail.com>
2024-07-05 10:20:03 +0530

    first commit

---
+++ src/main/java/com/microservice/ms_architecture_parent/MsArchitectureParentApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.ms_architecture_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsArchitectureParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsArchitectureParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/ms_architecture_parent/MsArchitectureParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.ms_architecture_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsArchitectureParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
